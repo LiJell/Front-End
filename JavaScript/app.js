3665fc69394046671e4dce086018afe2aebd5c14
LiJell <hanjukim0922@gmail.com>
2022-05-13 17:58:34 +0900

    const_let_difference

---
+++ JavaScript/app.js
@@ -0,0 +1,15 @@
+// const 와 let의 차이점
+// const -> cannot changed
+// let -> can be changed later
+const a = 5;
+const b = 2;
+let myName = "Hanju";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello! " + myName);
+
+myName = "LiJell";
+
+console.log("your new name is " + myName);
