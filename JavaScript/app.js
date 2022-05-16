6f0e2983b8ec5ab4122389d839fb3a8f0f8e040f
LiJell <hanjukim0922@gmail.com>
2022-05-17 00:53:58 +0900

    Array

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,9 @@
-const amIFat= null;
-// amIFat에 아무것도 채워지지 않은 null 채우는것을 의미
-let something;
-console.log(something);
+const daysOfWeek = ['mon','tue','wed','thu','fri','sat'];
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
