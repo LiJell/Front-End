646d7b7472f27c16cd19027e1a17f10a752625b7
LiJell <hanjukim0922@gmail.com>
2022-05-18 00:11:09 +0900

    anchor_control

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+// link의 기본 동작은 페이지 이동이다
