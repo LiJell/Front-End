2e2f2d1772f033aa21e4ff5f82b99facd0c6f411
LiJell <hanjukim0922@gmail.com>
2022-05-17 16:04:21 +0900

    conditional_2nd

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,8 +2,12 @@
 // parseInt is a converter: string to number
 const age = parseInt(prompt("How old are you?"));
 
-if (isNaN(age)){
-    console.log("Please write a number");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a positive number");
+} else if (age < 18) {
+  console.log("you are too young to drink");
+} else if (age >= 18 && age <= 50) {
+  console.log("you can drink");
 } else {
-    console.log("Thank you for writing your age")
+  console.log("better to exercise");
 }
