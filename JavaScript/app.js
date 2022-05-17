44d589eed1d80c60fbb5a6c87f365a8f3664732e
LiJell <hanjukim0922@gmail.com>
2022-05-17 17:11:33 +0900

    HTML_in_Javascript

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,13 +1,6 @@
-// 요즘은 prompt 잘 안씀
-// parseInt is a converter: string to number
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a positive number");
-} else if (age < 18) {
-  console.log("you are too young to drink");
-} else if (age >= 18 && age <= 50) {
-  console.log("you can drink");
-} else {
-  console.log("better to exercise");
-}
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
