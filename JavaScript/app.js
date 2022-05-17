6347f0f35f2905c61ad2dc74fc5f281ce6b94790
LiJell <hanjukim0922@gmail.com>
2022-05-17 22:59:00 +0900

    roleOfToggle

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,18 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 // properties를 찾을 땐 console.dir 를 이용하고 browser console에서 확인하는 방법이 좋음
 
+// function handleTitleClick() {
+//   const clickedClass = "clicked";
+//   if (h1.classList.contains(clickedClass)) {
+//     h1.classList.remove(clickedClass);
+//   } else {
+//     h1.classList.add(clickedClass);
+//   }
+// }
+// 위 코드는 toggle로 대체
+
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor;
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
