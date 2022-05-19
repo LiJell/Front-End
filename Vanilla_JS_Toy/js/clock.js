8d9bc271406de4495294fa6d1609b5dba72f3152
LiJell <hanjukim0922@gmail.com>
2022-05-20 00:23:29 +0900

    clock_using_padStart

--- Vanilla_JS_Toy/js/clock.js
+++ Vanilla_JS_Toy/js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2,"0");
+  const minutes = String(date.getMinutes()).padStart(2,"0");
+  const seconds = String(date.getSeconds()).padStart(2,"0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 // argument 1: function that I want to run, argument 2: how often (ms)
