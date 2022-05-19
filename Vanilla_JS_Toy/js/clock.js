35fe4dd647a6e8da17d41a5175247b191a408bb8
LiJell <hanjukim0922@gmail.com>
2022-05-19 23:52:51 +0900

    interval

---
+++ Vanilla_JS_Toy/js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello() {
+  console.log("hello");
+}
+
+// argument 1: function that I want to run, argument 2: how often (ms)
+setInterval(sayHello, 5000);
