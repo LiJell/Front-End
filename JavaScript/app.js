dafaedf57d595e15582c57a3b70a6d406a35396d
LiJell <hanjukim0922@gmail.com>
2022-05-19 23:00:20 +0900

    Using_local_storage_API

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -4,14 +4,30 @@ const greeting = document.querySelector("#greeting");
 
 //string 만 포함될 땐 대문자로 하는게 관습
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   // greeting.innerText = "Hello " + username; 아래와 동일하게 동작
-  greeting.innerText = `Hello ${username}`
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUserName);
+}
