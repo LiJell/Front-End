396d3bb1cc9c30d9740ef401dab598f8c6b4a144
LiJell <hanjukim0922@gmail.com>
2022-11-17 23:20:59 +0900

    useEffect

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,20 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("once called");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back React!!</h1>
-      <Button text={"hit me up"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button onClick={onClick} text={"hit me up"} />
     </div>
   );
 }
